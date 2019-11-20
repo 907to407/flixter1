00f1fdcaf676b62ebf2126567fe108aa35cbc102
Kimberly Conkle <kimberlyconkle88@gmail.com>
2019-11-20 23:39:22 +0000

    make it better

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,9 +1,11 @@
 class Instructor::CoursesController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :require_authorized_for_current_course, only: [:show]
+
   def new
     @course = Course.new
   end
+
   def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
@@ -12,12 +14,22 @@ class Instructor::CoursesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-  
+
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def require_authorized_for_current_course
+    if current_course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
